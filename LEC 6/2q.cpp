7d67b460b0a47e566910e542b3fc0749c8dc6865
HarshaMangipudi06 <148687778+HarshaMangipudi06@users.noreply.github.com>
2025-12-28 17:02:40 +0530

    Add files via upload

---
+++ LEC 6/2q.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[50];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    //prestore
+
+
+
+    //fetch
+    int input;
+    cin >> input;
+    for(int i=0; i<n; i++){
+        if(input == arr[i]){
+            
+
+        }
+    
+    }
+
+
+
+}
